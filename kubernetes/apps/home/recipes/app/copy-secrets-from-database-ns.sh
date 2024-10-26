ac4199ee98dd9514267d5e0a4ddc99a5102de008
Leeward Bound <leeward@boundcorp.net>
2024-10-26 15:37:14 -0700

    update homelab vars

--- kubernetes/apps/home/recipes/app/copy-secrets-from-database-ns.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl get secret postgres-pguser-recipes -n database -o yaml \
-  | sed 's/namespace: database/namespace: home/' \
-  | yq eval 'del(.metadata.ownerReferences)' - \
-  | kubectl apply -f -
